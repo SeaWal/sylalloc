d773461c55b5e65a3ab2a52c4f7fca3d4df2dbbc
SeaWal <sean.walshe@tutanota.com>
2026-02-12 18:57:14 +0000

    very basic test. TODO: implement basic test suite for sylalloc

--- tests/test_basic.c
+++ tests/test_basic.c
@@ -1,3 +1,13 @@
+#include <assert.h>
+#include <stdio.h>
+
+#include "sylalloc.h"
+
 int main() {
-    
+    void* p = syl_malloc(32);
+    if(p == NULL) {
+        printf("failed to allocate\n");
+    } else {
+        printf("successfully allocated\n");
+    }
 }

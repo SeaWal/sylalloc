e15e9745c2ac57917488551662eb379058922155
SeaWal <sean.walshe@tutanota.com>
2026-02-26 18:01:08 +0000

    Created basic unit tests: - test_basic_alloc: assert syl_malloc returns non-null useable pointer - test_free: assert syl_free sets pointer to null - test_alloc_zero_returns_null: assert that we don't alloc memory when 0 bytes requested - test_free_null_no_crash: assert the program doesnt crash if we try to free a null pointer - test_reuse_freed_mem: assert that the allocator reuses recently freed memory when allocating

--- tests/test.c
+++ tests/test.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "sylalloc.h"
 
 #define FAIL 0
 #define PASS 1
@@ -23,10 +24,64 @@
 static void test_basic_alloc() {
     TEST_INIT("test_basic_alloc");
 
+    void* p = syl_malloc(32);
+    ASSERT(p != NULL);
+
+    syl_free(p);
+
+    TEST_END(PASS);
+}
+
+static void test_free() {
+    TEST_INIT("test_free");
+
+    void* p = syl_malloc(32);
+    syl_free(p);
+    ASSERT(p == NULL);
+
+    TEST_END(PASS);
+}
+
+static void test_alloc_zero_returns_null() {
+    TEST_INIT("test_alloc_zero_returns_null");
+
+    void* p = syl_malloc(0);
+    ASSERT(p == NULL);
+
+    syl_free(p);
+
+    TEST_END(PASS);
+}
+
+static void test_free_null_no_crash() {
+    TEST_INIT("test_free_null_no_crash");
+
+    syl_free(NULL);
+
+    TEST_END(PASS);
+}
+
+static void test_reuse_freed_mem() {
+    TEST_INIT("test_reuse_freed_mem");
+
+    void* p1 = syl_malloc(32);
+    ASSERT(p1 != NULL);
+    syl_free(p1);
+
+    void* p2 = syl_malloc(32);
+    ASSERT(p2 != NULL);
+    ASSERT(p1 == p2);
+
+    syl_free(p2);
+
     TEST_END(PASS);
 }
 
 
 int main() {
     test_basic_alloc();
+    test_free();
+    test_alloc_zero_returns_null();
+    test_free_null_no_crash();
+    test_reuse_freed_mem();
 }

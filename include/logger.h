648aea339f12bda94f14c9d7ba33b879fd81ba7b
SeaWal <sean.walshe@tutanota.com>
2026-03-21 18:08:18 +0000

    changed LOG macro to allow variadic args and removed +1 from line count

--- include/logger.h
+++ include/logger.h
@@ -12,6 +12,7 @@ int log_init(struct Logger* logger, const char* log_filename);
 void log_close(struct Logger* logger);
 void log_msg(struct Logger* logger, const char* msg, ...);
 
-#define LOG(logger, msg) log_msg((logger), "%s:%d (%s): %s", __FILE__, __LINE__+1, __func__, (msg))
+#define LOG(logger, msg, ...) \
+    log_msg((logger), "%s:%d (%s): %s", __FILE__, __LINE__, __func__, (msg), ##__VA_ARGS__)
 
 #endif
